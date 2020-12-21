2bfe0c5118261b27a75a119e048d0943d9ad145f
momoisgoodforhealth <shoban.shakya@gmail.com>
2020-12-21 13:21:20 +0545

    testing of bookings tracking

--- app/src/main/java/com/romp/khel/Booking.kt
+++ app/src/main/java/com/romp/khel/Booking.kt
@@ -1,32 +1,26 @@
 package com.romp.khel
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.firebase.database.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Booking.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Booking : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    var database = FirebaseDatabase.getInstance().getReference()
+    var conditionref: DatabaseReference = database.child("check")
+
+    lateinit var timing1:TextView
+    lateinit var timing2:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -37,23 +31,25 @@ class Booking : Fragment() {
         return inflater.inflate(R.layout.fragment_booking, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Booking.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Booking().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        timing1=view.findViewById(R.id.time1)
+        timing2=view.findViewById(R.id.time2)
+
+        conditionref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (postSnapshot in dataSnapshot.children) {
+                    if (postSnapshot.key=="check1" && postSnapshot.getValue().toString()=="1") { timing1.setTextColor(Color.parseColor("#46EC2F")) }
+                    if (postSnapshot.key=="check1" && postSnapshot.getValue().toString()=="0") { timing1.setTextColor(Color.parseColor("#EC2F2F")) }
+                    if (postSnapshot.key=="check2" && postSnapshot.getValue().toString()=="1") { timing2.setTextColor(Color.parseColor("#46EC2F")) }
+                    if (postSnapshot.key=="check2" && postSnapshot.getValue().toString()=="0") { timing2.setTextColor(Color.parseColor("#EC2F2F")) }
                 }
+
             }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
     }
 }
